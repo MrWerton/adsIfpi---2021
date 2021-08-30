1de1e7e154776a989686f66b68de5035092cf47a
Werton Pereira <84238829+MrWerton@users.noreply.github.com>
2021-08-30 05:07:20 -0300

    Add files via upload

---
+++ ESTRUTURA DE DADOS/LISTA03/Exe03.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+  /*
+    int tamanhoA, tamanhoB, i, j,n;
+    printf("Informe o tamanho do vetorA: ");
+    scanf("%d", &tamanhoA);
+    printf("Informe o tamanho do vetorB: ");
+    scanf("%d", &tamanhoB);
+    
+    int VetorA[tamanhoA], VetorB[tamanhoB];
+
+    for(i = 0; i < tamanhoA; i++) {
+        printf("Digite o elemento %d: do vetorA: ",i);
+        scanf("%d", &VetorA[i]);
+    };
+     for(j = 0; j < tamanhoB; j++) {
+        printf("Digite o elemento %d do vetor B: ",j);
+        scanf("%d", &VetorB[j]);
+    }
+
+    int interseccao[0];
+    printf("Interseccao [");
+    for(i = 0; i < tamanhoA; i++){
+        for(j = 0; j < tamanhoB ; j++){
+            if(VetorA[i] == VetorB[j]){
+                printf(" %d ", VetorA[i]);               
+                printf(" %d ", VetorB[i]);               
+             };
+        };
+    };
+    int VetorC = VetorB+VetorA;
+    printf("]\n");
+    int Uniao[0];
+    int Uniao_1[0];
+    int Uniao_2[0];
+    printf("União [");
+
+   for(n = 0; n < tamanhoC; n++) {
+        printf(" %d ",VetorC[n]);
+
+      };
+    printf("}");
+    printf("]\n");
+
+    return 0;
+    */
+   int tamanhoA, tamanhoB, i, j,n;
+    printf("Informe o tamanho do vetorA: ");
+    scanf("%d", &tamanhoA);
+    printf("Informe o tamanho do vetorB: ");
+    scanf("%d", &tamanhoB);
+    int VetorA[tamanhoA], VetorB[tamanhoB];
+    int tamanhoC = tamanhoA + tamanhoB;
+    int Uniao[tamanhoC];
+    for(i = 0; i < tamanhoA; i++) {
+        printf("Digite o elemento %d: do vetorA: ",i);
+        scanf("%d", &VetorA[i]);
+         Uniao[i] = VetorA[i];
+    }
+    for(j = 0; j < tamanhoB; j++) {
+        printf("Digite o elemento %d do vetor B: ",i);
+        scanf("%d", &VetorB[j]);
+         Uniao[i] = VetorB[j];
+          i++;
+    }
+    printf("União é: {");
+
+      for(n = 0; n < tamanhoC; n++) {
+          for(int m = tamanhoC; m >= 0; m--){
+            if(Uniao[m] != Uniao[n]){
+              printf(" %d ",Uniao[n]);
+              break;
+            };
+          };
+
+      };
+    printf("}\n");
+     int interseccao[0];
+    printf("Interseccao [");
+    for(i = 0; i < tamanhoA; i++){
+        for(j = 0; j < tamanhoB ; j++){
+            if(VetorA[i] == VetorB[j]){
+                printf(" %d ", VetorA[i]);                             
+             };
+        };
+    };
+    printf("]\n");
+    return 0;
+}
